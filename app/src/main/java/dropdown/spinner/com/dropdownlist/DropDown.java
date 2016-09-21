a1aa6b5d28d1b9e3a717dfd6afbe8dec04bf79a3
sarath.kn <sarath.kn@robosoftin.com>
2016-09-21 21:12:54 +0530

    added popup changes

--- app/src/main/java/dropdown/spinner/com/dropdownlist/DropDown.java
+++ app/src/main/java/dropdown/spinner/com/dropdownlist/DropDown.java
@@ -6,15 +6,20 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +32,11 @@ public class DropDown<T> extends EditText implements View.OnClickListener {
     private ListView mListView;
     private int popupHeight = 500;
     private int backgroundColor = Color.WHITE;
+    private LinearLayout linearLayout;
+    private TextView headerTextView;
+    private static final int sPaddingLeft = 8;
+    private static final int sPaddingRight = 8;
+
 
     public interface ItemClickListener<T> {
         void onItemSelected(DropDown dropDown, T selectedItem);
@@ -50,11 +60,32 @@ public class DropDown<T> extends EditText implements View.OnClickListener {
     }
 
     private void init(AttributeSet attrs) {
-        Drawable img = getContext().getResources().getDrawable(R.drawable.ic_arrow_drop_down_black_24dp);
-        img.setBounds(0, 0, 80, 80);
-        setCompoundDrawables(null, null, img, null);
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+        popupHeight = height / 3;
+
+        Drawable arrowDrawable = ContextCompat.getDrawable(getContext(), R.drawable.ms__arrow);
+
+        final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        setLayoutParams(layoutParams);
+        layoutParams.setMargins(sPaddingLeft, 0, sPaddingRight, 0);
+        layoutParams.gravity = Gravity.CENTER_VERTICAL;
+
+        arrowDrawable.setBounds(0, 0, 80, 80);
+        setCompoundDrawables(null, null, arrowDrawable, null);
         setKeyListener(null);
         setOnClickListener(this);
+        linearLayout = new LinearLayout(getContext());
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        headerTextView = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.simple_list_item_1, null);
+        final LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams1.setMargins(sPaddingLeft, 0, sPaddingRight, 0);
+        headerTextView.setLayoutParams(layoutParams1);
+
+        headerTextView.setCompoundDrawables(null, null, arrowDrawable, null);
         mListView = new ListView(getContext());
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -71,8 +102,10 @@ public class DropDown<T> extends EditText implements View.OnClickListener {
 
         final ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         mListView.setLayoutParams(params);
+        linearLayout.addView(headerTextView);
+        linearLayout.addView(mListView);
         popupWindow = new PopupWindow(getContext());
-        popupWindow.setContentView(mListView);
+        popupWindow.setContentView(linearLayout);
         popupWindow.setOutsideTouchable(true);
         popupWindow.setFocusable(true);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -90,6 +123,7 @@ public class DropDown<T> extends EditText implements View.OnClickListener {
 
     private void collapse() {
         popupWindow.dismiss();
+
     }
 
     public void setItems(List<T> items) {
@@ -112,7 +146,7 @@ public class DropDown<T> extends EditText implements View.OnClickListener {
             if (popupHeight > totalHeight) {
                 popupHeight = totalHeight;
             }
-
+            headerTextView.setText("Select an item");
             final int[] location = new int[2];
             getLocationInWindow(location);
             popupWindow.setHeight(popupHeight);
@@ -120,6 +154,10 @@ public class DropDown<T> extends EditText implements View.OnClickListener {
             popupWindow.showAtLocation(this, Gravity.NO_GRAVITY, location[0], location[1]);
         }
 
+    }
+
+    public T getSelectedItem() {
+        return mSelectedItem;
     }
 
     @Override
